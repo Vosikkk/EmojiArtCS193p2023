8ab3aa68bf1ca7ca0148bde4784f7fb984f2ce8f
Vosik <vosikvos@gmail.com>
2024-01-22 00:33:58 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Саша Восколович on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
