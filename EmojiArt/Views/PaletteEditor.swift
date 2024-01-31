a2dc8f07ba1074fa776230357e67388c374d627c
Vosik <vosikvos@gmail.com>
2024-01-31 18:30:14 +0200

    added palette editor

---
+++ EmojiArt/Views/PaletteEditor.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteEditor.swift
+//  EmojiArt
+//
+//  Created by Саша Восколович on 31.01.2024.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteEditor()
+}
